0a2c1ea9639a3f6a74220ad9600ef70035cbe297
Shuchita Mahey <shuchitamahey@Shuchitas-MBP.hitronhub.home>
2020-07-10 12:49:22 -0700

    add json web token for verification

--- routes/auth.js
+++ routes/auth.js
@@ -46,7 +46,11 @@ router.post('/login', async(req,res) => {
   const validPass = await bcrypt.compare(req.body.password, user.password);
   if(!validPass) return res.status(400).send('Invalid password')
 
-  res.send("Logged in!")
+  //create and assign a token
+  const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET)
+  res.header('auth-token', token).send(token);
+
+  // res.send("Logged in!")
   })
 
 
