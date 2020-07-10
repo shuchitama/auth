9c8c14251fe790cbf9f5d7a7a63342ca7bcb3ac7
Shuchita Mahey <shuchitamahey@Shuchitas-MBP.hitronhub.home>
2020-07-10 12:01:06 -0700

    hash passwords

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const User = require('../model/User');
+const bcrypt = require('bcryptjs');
 const {registerValidation, loginValidation} = require('../validation')
-
 router.post('/register', async (req, res) => {
 
   //Validate data before making a user
@@ -12,19 +12,26 @@ router.post('/register', async (req, res) => {
   const emailExists = await User.findOne({email: req.body.email});
   if(emailExists) return res.status(400).send('Email already exists!')
 
+  //Hash passwords
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+
   const user = new User({
     name: req.body.name,
     email: req.body.email,
-    password: req.body.password,
+    password: hashedPassword,
   });
   try {
     const savedUser = await user.save();
-    res.send(savedUser);
+    res.send({ user: user._id });
   } catch(err) {
     res.status(400).send(err)
   }
 
 });
+
+
 
 
 module.exports = router;
