e23fa307e399836032fea3531791f758de7c4989
Shuchita Mahey <shuchitamahey@Shuchitas-MBP.hitronhub.home>
2020-07-10 13:05:10 -0700

    create private route with jwt

--- index.js
+++ index.js
@@ -4,19 +4,23 @@ const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 //Import Routes
 const authRoute = require('./routes/auth');
+const postRoute = require('./routes/posts');
 
 dotenv.config();
 
 //Connect to DB
 mongoose.connect(
   process.env.DB_CONNECT,
-{ useUnifiedTopology: true,
-  useNewUrlParser: true },
-() => {console.log('Connected to database!')})
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  },
+  () => { console.log('Connected to database!') })
 
 //body-parser: lets you read from req.body
 app.use(express.json());
 //Route Middlewares
 app.use('/api/user', authRoute);  //prefix for all routes in auth.js
+app.use('/api/posts', postRoute);  //prefix for all routes in posts.js
 
 app.listen(3000, () => console.log('Server up and running!'));
