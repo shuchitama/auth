0fff9902b0c905991b48d0f387601b9e4edb0c7e
Shuchita Mahey <shuchitamahey@Shuchitas-MBP.hitronhub.home>
2020-07-09 22:47:18 -0700

    add register and login validations

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,13 @@
 const router = require('express').Router();
 const User = require('../model/User');
+const {registerValidation, loginValidation} = require('../validation')
 
 router.post('/register', async (req, res) => {
+
+  //Validate data before making a user
+  const {error} = registerValidation(req.body);
+  if(error) return res.status(400).send(error.details[0].message)
+
   const user = new User({
     name: req.body.name,
     email: req.body.email,
