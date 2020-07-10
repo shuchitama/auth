854be7e3dcf2d519a25590431479a3b72823e9f8
Shuchita Mahey <shuchitamahey@Shuchitas-MBP.hitronhub.home>
2020-07-09 22:50:57 -0700

    check for unique email

--- routes/auth.js
+++ routes/auth.js
@@ -8,6 +8,10 @@ router.post('/register', async (req, res) => {
   const {error} = registerValidation(req.body);
   if(error) return res.status(400).send(error.details[0].message)
 
+  //check if user is already in database
+  const emailExists = await User.findOne({email: req.body.email});
+  if(emailExists) return res.status(400).send('Email already exists!')
+
   const user = new User({
     name: req.body.name,
     email: req.body.email,
